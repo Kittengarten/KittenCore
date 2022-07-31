094e8c91aa2c756adf85f90dee90a7f1b33631c5
Kittengarten <kittengarten@vip.qq.com>
2022-07-31 11:14:46 +0800

    新增精华消息插件（因为API限制，仅支持获取最近20条，且过于久远的消息无法获取）

---
+++ essence/essence.go
@@ -0,0 +1,55 @@
+package essence
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	"github.com/Kittengarten/KittenCore/kitten"
+	"github.com/tidwall/gjson"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	// ReplyServiceName 插件名
+	ReplyServiceName = "精华消息"
+)
+
+func init() {
+	config := kitten.LoadConfig()
+	help := strings.Join([]string{"发送",
+		fmt.Sprintf("%s精华", config.CommandPrefix),
+		"随机抽取一条本群的精华消息",
+	}, "\n")
+	// 注册插件
+	engine := control.Register(ReplyServiceName, &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: false,
+		Help:             help,
+	})
+
+	engine.OnCommand("精华").Handle(func(ctx *zero.Ctx) {
+		essenceList := ctx.GetThisGroupEssenceMessageList()
+		essenceCount := len(essenceList.Array())
+		log.Tracef("获得了 %d 条精华消息喵！", essenceCount)
+		IDx := rand.Intn(int(essenceCount))
+		essenceMessage := essenceList.Array()[IDx]
+		log.Trace(essenceMessage)
+		var (
+			ID       = gjson.Get(essenceMessage.Raw, "sender_id")
+			nickname = gjson.Get(essenceMessage.Raw, "sender_nick")
+			msID     = gjson.Get(essenceMessage.Raw, "message_id")
+		)
+		ms := ctx.GetMessage(message.NewMessageIDFromInteger(msID.Int()))
+		log.Trace(ms)
+		reportText := kitten.TextOf("【精华消息】\n%s（%d）:\n", nickname.String(), ID.Int())
+		report := make(message.Message, len(ms.Elements))
+		report = append(report, reportText)
+		report = append(report, ms.Elements...)
+		ctx.Send(report)
+	})
+}
